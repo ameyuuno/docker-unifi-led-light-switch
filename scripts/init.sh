37092deb10ac1e0c6dc89f05dd286e4932801927
ameyuuno <ameyuuno@gmail.com>
2020-09-01 12:17:35 +0300

    create init script which generate crontab schedule file

---
+++ scripts/init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Generate crontab schedule file."
+
+led_switch_on_schedule=${UNIFI_LED_SWITCH_ON_CRONTAB:-"0 7 * * *"}
+led_switch_off_schedule=${UNIFI_LED_SWITCH_OFF_CRONTAB:-"0 0 * * *"}
+
+echo "${led_switch_on_schedule} unifi-led-switch on" >> /srv/unifi-led-switch-crontab-schedule
+echo "${led_switch_off_schedule} unifi-led-switch off" >> /srv/unifi-led-switch-crontab-schedule
+
+echo "Schedule:"
+echo "---------"
+cat /srv/unifi-led-switch-crontab-schedule
+echo "---------"
+
+echo "Done."
